12796a9512e7ef9aaa2401de811c6f834c73681b
liofan <jakanov.andr@gmail.com>
2018-05-22 05:16:04 +0600

    Я закончил верстку проекта по PSD макету на FlexBox

--- README.md
+++
@@ -1,4 +0,0 @@
-# google-homepage
-http://codenamecrud.ru/basics-of-web-development/project-html-css
-Мой первый учебныый проект который я залью на GitHub
-Первый проект в котором я использую GitHub впредь я вче свои проекты буду заливать
--- js/js.js
+++ js/js.js
@@ -7,7 +7,14 @@ $(function(){
 		$('#info__box').toggleClass('open');  
 	});
 
+	$('.pr__bar').each(function(){
+		let width = $(this).data('skill'); //В переменую записали необходимую ширину
+		$(this).css('width', width + "%");
+	});
+
+	$('#menu__btn').click( function() {
+		$('#nav__main').toggleClass('open');  
+	});
 
 });
 
-// агростек миханик 1год 
